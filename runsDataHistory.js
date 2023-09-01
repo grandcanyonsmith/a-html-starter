47b6ef0bef5bbdc3249d7320cb70fc349f10e401
Canyon Smith <canyonfsmith@gmail.com>
2023-09-01 15:33:54 -0600

    Update runsDataHistory.js

--- runsDataHistory.js
+++ runsDataHistory.js
@@ -62,48 +62,52 @@ $(document).ready(function() {
     }
   };
 
-  const handleTestRunsResponse = (response) => {
-    elements.pastRunsBody.empty();
-    response.forEach((testRun, index) => {
-      const statusIcon =
-        testRun.Status === "Passed" ?
-        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill text-green-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>' :
-        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill text-red-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg>';
-      const row = `<tr>
-              <td class="p-2 border border-gray-800 text-gray-300">
-                  <div class="text-white">${testRun.Date.split(" ")[0]}</div>
-                  <div class="text-gray-400 text-xs">${
-                    testRun.Date.split(" ")[1]
-                  }</div>
-              </td>
-              <td class="p-2 border border-gray-800 test-id hover:bg-gray-700 cursor-pointer">
-                  <a href="#" class="text-white no-underline">${
-                    testRun.TestID
-                  }</a>
-              </td>
-              <td class="p-2 border border-gray-800 status flex justify-center items-center">${statusIcon} ${
-      testRun.Status
-    }</td>
-              <td class="p-2 border border-gray-800 text-gray-300">${
-                testRun.Duration
-              }</td>
-              <td class="p-2 border border-gray-800 text-gray-300">${
-                testRun.Tests
-              }</td>
-              <td class="p-2 border border-gray-800 text-gray-300">${
-                testRun.Passed
-              }</td>
-              <td class="p-2 border border-gray-800 text-gray-300">${
-                testRun.Failed
-              }</td>
-              <td class="p-2 border border-gray-800 text-green-500">${
-                testRun.Efficiency
-              }</td>
-          </tr>`;
-      elements.pastRunsBody.append(row);
+const handleTestRunsResponse = (response) => {
+  elements.pastRunsBody.empty();
+  response.forEach((testRun, index) => {
+    const statusIcon =
+      testRun.Status === "Passed" ?
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill text-green-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>' :
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill text-red-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg>';
+    const row = $(`<tr>
+            <td class="p-2 border border-gray-800 text-gray-300">
+                <div class="text-white">${testRun.Date.split(" ")[0]}</div>
+                <div class="text-gray-400 text-xs">${
+                  testRun.Date.split(" ")[1]
+                }</div>
+            </td>
+            <td class="p-2 border border-gray-800 test-id hover:bg-gray-700 cursor-pointer">
+                <a href="#" class="text-white no-underline">${
+                  testRun.TestID
+                }</a>
+            </td>
+            <td class="p-2 border border-gray-800 status flex justify-center items-center">${statusIcon} ${
+    testRun.Status
+  }</td>
+            <td class="p-2 border border-gray-800 text-gray-300">${
+              testRun.Duration
+            }</td>
+            <td class="p-2 border border-gray-800 text-gray-300">${
+              testRun.Tests
+            }</td>
+            <td class="p-2 border border-gray-800 text-gray-300">${
+              testRun.Passed
+            }</td>
+            <td class="p-2 border border-gray-800 text-gray-300">${
+              testRun.Failed
+            }</td>
+            <td class="p-2 border border-gray-800 text-green-500">${
+              testRun.Efficiency
+            }</td>
+        </tr>`);
+    row.find('.test-id a').on('click', function(e) {
+      e.preventDefault();
+      window.location.href = `testData.html?testID=${testRun.TestID}`; // Replace 'testData.html' with the actual page URL
     });
-    updateAggregateValues();
-  };
+    elements.pastRunsBody.append(row);
+  });
+  updateAggregateValues();
+};
 
   const generateGraph = (testRuns) => {
     const labels = testRuns.map(run => run.Date.split(" ")[0].substring(5)); //remove the year with substring the first 5 characters
