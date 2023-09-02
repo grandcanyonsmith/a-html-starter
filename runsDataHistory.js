c4b97544abadfedfe822083f6177985f156fb294
Canyon Smith <canyonfsmith@gmail.com>
2023-09-01 18:07:20 -0600

    Update runsDataHistory.js

--- runsDataHistory.js
+++ runsDataHistory.js
@@ -69,7 +69,7 @@ const handleTestRunsResponse = (response) => {
       testRun.Status === "Passed" ?
       '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill text-green-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>' :
       '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill text-red-500 mr-2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg>';
-    const row = $(`<tr>
+const row = $(`<tr>
             <td class="p-2 border border-gray-800 text-gray-300">
                 <div class="text-white">${testRun.Date.split(" ")[0].substring(5)}</div>
                 <div class="text-gray-400 text-xs">${
