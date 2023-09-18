b1a011cd61f200458b47c9c4d8369cf8a516f081
Canyon Smith <canyonfsmith@gmail.com>
2023-09-18 16:00:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ window.onload = function() {
         }), {});
 
     const urls = {
-        update: 'https://flask-hello-world2-three.vercel.app/update',
+  runCode: "https://xhy5at2dbpxeys62rsx4f6lfay0yigqt.lambda-url.us-west-2.on.aws/",
+  submitRequest: "https://uslbd6l6ssolgomdrcdhnqa5me0rnsee.lambda-url.us-west-2.on.aws/",
+  getRepos: "https://flask-hello-world2-three.vercel.app/github_repos_list",
+  getFiles: "https://flask-hello-world2-git-main-grandcanyonsmith.vercel.app/github_repos/",
+  update: "https://flask-hello-world2-three.vercel.app/update",
         create: 'https://flask-hello-world2-three.vercel.app/create_new_file'
-    };
+};
 
     const addEventListeners = () => {
         Object.entries(elements).forEach(([key, element]) => {
