51975fc2bddd3d7e6aec3526a1bf462f2e97a6ed
Canyon Smith <canyonfsmith@gmail.com>
2023-09-18 07:55:49 -0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,302 @@
+let questions = [
+    {
+        "id": 1,
+        "question": "What is the purpose or objective of the business or project?"
+    },
+    {
+        "id": 2,
+        "question": "What is the vision of the company?"
+    },
+    {
+        "id": 3,
+        "question": "What problem does the project aim to solve?"
+    },
+    {
+        "id": 4,
+        "question": "Who are the key stakeholders involved in this project?"
+    },
+    {
+        "id": 5,
+        "question": "Who are the decision-makers in the project?"
+    },
+    {
+        "id": 6,
+        "question": "Who will be directly affected by the project's outcome?"
+    },
+    {
+        "id": 7,
+        "question": "What are the specific goals and objectives of the project?"
+    },
+    {
+        "id": 8,
+        "question": "What are the short-term and long-term goals?"
+    },
+    {
+        "id": 9,
+        "question": "How do these goals align with the company's overall objectives?"
+    },
+    {
+        "id": 10,
+        "question": "What are the key business processes that will be affected by this project?"
+    },
+    {
+        "id": 11,
+        "question": "What are the current business processes?"
+    },
+    {
+        "id": 12,
+        "question": "How will these processes change after the project?"
+    },
+    {
+        "id": 13,
+        "question": "What are the expected deliverables of the project?"
+    },
+    {
+        "id": 14,
+        "question": "What are the tangible and intangible outputs of the project?"
+    },
+    {
+        "id": 15,
+        "question": "When are these deliverables expected?"
+    },
+    {
+        "id": 16,
+        "question": "What are the functional requirements of the project?"
+    },
+    {
+        "id": 17,
+        "question": "What are the specific functionalities that the project should deliver?"
+    },
+    {
+        "id": 18,
+        "question": "How will these functionalities address the project's objectives?"
+    },
+    {
+        "id": 19,
+        "question": "What are the non-functional requirements of the project?"
+    },
+    {
+        "id": 20,
+        "question": "What are the performance, security, and usability requirements?"
+    },
+    {
+        "id": 21,
+        "question": "How will these requirements impact the project's success?"
+    },
+    {
+        "id": 22,
+        "question": "What are the technical requirements of the project?"
+    },
+    {
+        "id": 23,
+        "question": "What technologies will be used in the project?"
+    },
+    {
+        "id": 24,
+        "question": "What are the hardware and software requirements?"
+    },
+    {
+        "id": 25,
+        "question": "What are the constraints or limitations of the project?"
+    },
+    {
+        "id": 26,
+        "question": "What are the budgetary, time, or resource constraints?"
+    },
+    {
+        "id": 27,
+        "question": "How will these constraints affect the project's execution?"
+    },
+    {
+        "id": 28,
+        "question": "What are the risks involved in the project?"
+    },
+    {
+        "id": 29,
+        "question": "What potential problems could arise during the project?"
+    },
+    {
+        "id": 30,
+        "question": "How will these risks be mitigated?"
+    },
+    {
+        "id": 31,
+        "question": "What is the expected timeline for the project?"
+    },
+    {
+        "id": 32,
+        "question": "When is the project expected to start and end?"
+    },
+    {
+        "id": 33,
+        "question": "What are the milestones and their deadlines?"
+    },
+    {
+        "id": 34,
+        "question": "What is the budget for the project?"
+    },
+    {
+        "id": 35,
+        "question": "What is the total cost of the project?"
+    },
+    {
+        "id": 36,
+        "question": "How will the budget be allocated?"
+    },
+    {
+        "id": 37,
+        "question": "Who will be the end-users of the product or service?"
+    },
+    {
+        "id": 38,
+        "question": "Who are the target customers or users?"
+    },
+    {
+        "id": 39,
+        "question": "What are their needs and expectations?"
+    },
+    {
+        "id": 40,
+        "question": "What are the user requirements?"
+    },
+    {
+        "id": 41,
+        "question": "What features and functionalities do the users need?"
+    },
+    {
+        "id": 42,
+        "question": "How will the users interact with the product or service?"
+    },
+    {
+        "id": 43,
+        "question": "What are the regulatory requirements, if any?"
+    },
+    {
+        "id": 44,
+        "question": "What laws or regulations apply to the project?"
+    },
+    {
+        "id": 45,
+        "question": "How will compliance be ensured?"
+    },
+    {
+        "id": 46,
+        "question": "How will success be measured for this project?"
+    },
+    {
+        "id": 47,
+        "question": "What are the key performance indicators (KPIs)?"
+    },
+    {
+        "id": 48,
+        "question": "How will these KPIs be tracked and reported?"
+    },
+    {
+        "id": 49,
+        "question": "What are the data requirements?"
+    },
+    {
+        "id": 50,
+        "question": "What data will be collected, stored, and processed?"
+    },
+    {
+        "id": 51,
+        "question": "How will data privacy and security be ensured?"
+    },
+    {
+        "id": 52,
+        "question": "What are the system integration requirements?"
+    },
+    {
+        "id": 53,
+        "question": "What systems need to be integrated with the project?"
+    },
+    {
+        "id": 54,
+        "question": "How will these integrations be managed?"
+    },
+    {
+        "id": 55,
+        "question": "What are the security requirements?"
+    },
+    {
+        "id": 56,
+        "question": "What are the potential security risks?"
+    },
+    {
+        "id": 57,
+        "question": "How will these risks be mitigated?"
+    },
+    {
+        "id": 58,
+        "question": "What are the maintenance and support requirements?"
+    },
+    {
+        "id": 59,
+        "question": "What are the expected maintenance tasks?"
+    },
+    {
+        "id": 60,
+        "question": "What support will be needed after the project's completion?"
+    },
+    {
+        "id": 61,
+        "question": "Are there any specific training needs for the users?"
+    },
+    {
+        "id": 62,
+        "question": "What skills or knowledge do the users need to use the product or service?"
+    },
+    {
+        "id": 63,
+        "question": "How will this training be provided?"
+    },
+    {
+        "id": 64,
+        "question": "What are the expected changes in the business environment during and after the project?"
+    },
+    {
+        "id": 65,
+        "question": "What are the current market trends?"
+    },
+    {
+        "id": 66,
+        "question": "How might these trends affect the project?"
+    },
+    {
+        "id": 67,
+        "question": "What are the contingency plans in case of project failure or delays?"
+    },
+    {
+        "id": 68,
+        "question": "What are the potential causes of failure or delay?"
+    },
+    {
+        "id": 69,
+        "question": "What are the backup plans for these scenarios?"
+    },
+    {
+        "id": 70,
+        "question": "What are the requirements for documentation?"
+    },
+    {
+        "id": 71,
+        "question": "What documents need to be created and maintained?"
+    },
+    {
+        "id": 72,
+        "question": "Who will be responsible for documentation?"
+    },
+    {
+        "id": 73,
+        "question": "How will changes to requirements be managed during the project?"
+    },
+    {
+        "id": 74,
+        "question": "What is the process for handling changes to the project requirements?"
+    },
+    {
+        "id": 75,
+        "question": "Who will be responsible for managing these changes?"
+    }
+];
