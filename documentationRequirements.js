00970dd04cc29af0d96a7e6807419b5480c43296
Canyon Smith <canyonfsmith@gmail.com>
2023-09-30 10:47:38 -0600

    updated documentationRequirements.js

--- documentationRequirements.js
+++ documentationRequirements.js
@@ -1,22 +1,48 @@
 function populateDocumentationRequirements() {
-    let docRequirements = [
-        // Add documentation requirements here, for example:
-        // { title: 'Requirement 1', description: 'Description 1' },
-        // { title: 'Requirement 2', description: 'Description 2' }
-    ];
+    const data = {
+        "documents": [
+            {
+                "documentName": "Project Overview",
+                "description": "The project's objective is to develop a centralized platform for managing and executing tests for OC Tanner's Test suite. The vision is to provide a tool that enables managers to determine product readiness for shipping and allows test engineers to run and fix tests from a single location.",
+                "author": "John Doe"
+            },
+            // More documents...
+        ]
+    };
 
-    let parentElement = document.getElementById("brd"); // Or any other target element
-    docRequirements.forEach(requirement => {
-        let requirementElement = document.createElement('div');
-        let titleElement = document.createElement('h3');
-        let descriptionElement = document.createElement('p');
+    const container = document.getElementById('brd');
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const headerRow = document.createElement('tr');
 
-        titleElement.innerText = requirement.title;
-        descriptionElement.innerText = requirement.description;
-
-        requirementElement.appendChild(titleElement);
-        requirementElement.appendChild(descriptionElement);
-
-        parentElement.appendChild(requirementElement);
+    ['Document Name', 'Description', 'Author'].forEach(text => {
+        const th = document.createElement('th');
+        th.textContent = text;
+        headerRow.appendChild(th);
     });
+
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    data.documents.forEach(document => {
+        const row = document.createElement('tr');
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = document.documentName;
+        row.appendChild(nameCell);
+
+        const descriptionCell = document.createElement('td');
+        descriptionCell.textContent = document.description;
+        row.appendChild(descriptionCell);
+
+        const authorCell = document.createElement('td');
+        authorCell.textContent = document.author;
+        row.appendChild(authorCell);
+
+        tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    container.appendChild(table);
 }
