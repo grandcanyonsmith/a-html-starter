8069343f8713c5b7c0e77a9789a0a68e6b9d7026
Canyon Smith <canyonfsmith@gmail.com>
2023-09-30 10:38:36 -0600

    create new file

---
+++ successCriteria.js
@@ -0,0 +1,50 @@
+function populateSuccessCriteria() {
+    let successCriteriaTable = `
+        <h2>Success Criteria</h2>
+        <div class="scrollable-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Test Metric</th>
+                        <th>Description</th>
+                        <th>Current</th>
+                        <th>Goal</th>
+                        <th>% Change</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Setup time <i class="fas fa-clock"></i></td>
+                        <td>Time to prepare and initiate a test run</td>
+                        <td>30 min</td>
+                        <td>3 min</td>
+                        <td>90% <i class="fas fa-arrow-down red-arrow"></i> Reduction</td>
+                    </tr>
+                    <tr>
+                        <td>Responsibility Diagnosis <i class="fas fa-clock"></i></td>
+                        <td>Time to determine cause of broken test and assign responsibility</td>
+                        <td>15 min</td>
+                        <td>3 min</td>
+                        <td>80% <i class="fas fa-arrow-down red-arrow"></i> Reduction</td>
+                    </tr>
+                    <tr>
+                        <td>Running Tests <i class="fas fa-clock"></i></td>
+                        <td>Time to run an entire test suite</td>
+                        <td>1 hour</td>
+                        <td>20 min</td>
+                        <td>67% <i class="fas fa-arrow-down red-arrow"></i> Reduction</td>
+                    </tr>
+                    <tr>
+                        <td>Fix Bugs <i class="fas fa-clock"></i></td>
+                        <td>Time to fix a broken test</td>
+                        <td>30 min - 1 hour</td>
+                        <td>3-5 min</td>
+                        <td>90% <i class="fas fa-arrow-down red-arrow"></i> Reduction</td>
+                    </tr>
+                    <!-- The KPI success data will be inserted here -->
+                </tbody>
+            </table>
+        </div>
+    `;
+    document.getElementById('successCriteria').innerHTML = successCriteriaTable;
+}
