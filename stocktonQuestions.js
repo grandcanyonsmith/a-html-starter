f23c61b9e881baf71528934b299a1344c3b3b5a0
Canyon Smith <canyonfsmith@gmail.com>
2023-09-30 15:57:07 -0600

    updated stocktonQuestions.js

--- stocktonQuestions.js
+++ stocktonQuestions.js
@@ -10,5 +10,73 @@ let questions = [
     {
         "id": 3,
         "question": "What problem does your product or service solve for your customers?"
+    },
+    {
+        "id": 4,
+        "question": "What is the unique selling proposition of your course?"
+    },
+    {
+        "id": 5,
+        "question": "What are the main benefits of your course?"
+    },
+    {
+        "id": 6,
+        "question": "What is the price of your course?"
+    },
+    {
+        "id": 7,
+        "question": "Do you offer any discounts or promotions?"
+    },
+    {
+        "id": 8,
+        "question": "What is the format of your course (video, text, audio)?"
+    },
+    {
+        "id": 9,
+        "question": "How long does it take to complete the course?"
+    },
+    {
+        "id": 10,
+        "question": "Do you provide any certificates upon completion?"
+    },
+    {
+        "id": 11,
+        "question": "What is the level of your course (beginner, intermediate, advanced)?"
+    },
+    {
+        "id": 12,
+        "question": "What are the prerequisites for your course?"
+    },
+    {
+        "id": 13,
+        "question": "Who are the instructors of your course?"
+    },
+    {
+        "id": 14,
+        "question": "Do you offer any support during the course?"
+    },
+    {
+        "id": 15,
+        "question": "Do you have any testimonials or success stories from previous students?"
+    },
+    {
+        "id": 16,
+        "question": "What is your refund policy?"
+    },
+    {
+        "id": 17,
+        "question": "How can potential students contact you for more information?"
+    },
+    {
+        "id": 18,
+        "question": "Do you have any upcoming courses or events?"
+    },
+    {
+        "id": 19,
+        "question": "What differentiates your course from others in the market?"
+    },
+    {
+        "id": 20,
+        "question": "What are the main challenges your potential students face?"
     }
 ];
