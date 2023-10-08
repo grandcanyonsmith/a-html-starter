e3fb4affae532fe9ef8e7945f90fa73f38445b20
Canyon Smith <canyonfsmith@gmail.com>
2023-10-08 15:13:52 -0600

    Update repoFileSelector.js

--- repoFileSelector.js
+++ repoFileSelector.js
@@ -119,18 +119,3 @@ class RepoFileSelector {
 }
 
 const repoFileSelector = new RepoFileSelector();
-
-window.onload = () => {
-  document.getElementById('fetchFilesBtn').addEventListener('click', fetchFiles);
-  feather.replace();
-  document.getElementById('createNewFileButton').addEventListener('click', () => repoFileSelector.openCreateNewFileModal());
-  document.getElementById('submitCreateFileButton').addEventListener('click', () => repoFileSelector.submitFile());
-  document.getElementById('cancelButton').addEventListener('click', () => repoFileSelector.closeCreateNewFileModal());
-  const chevronIcon = document.getElementById('chevronIcon');
-  if (chevronIcon) {
-    chevronIcon.addEventListener('click', () => {
-      const selectedRepoName = document.getElementById('selectedRepoName').value;
-      repoFileSelector.fetchRepositoryContents(selectedRepoName);
-    });
-  }
-};
