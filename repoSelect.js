d87e180e4fdba573dbd6db102b2f7e4d510ad62e
Canyon Smith <canyonfsmith@gmail.com>
2023-10-08 14:56:54 -0600

    updated repoSelect.js

--- repoSelect.js
+++ repoSelect.js
@@ -1,97 +1,89 @@
-const API_ENDPOINT = 'https://nyk43gzspnm7wfhwqrc4uaprya0ecdap.lambda-url.us-west-2.on.aws/';
+window.onload = async () => {
+  const API_ENDPOINT = 'https://nyk43gzspnm7wfhwqrc4uaprya0ecdap.lambda-url.us-west-2.on.aws/';
 
-class GithubRepoManager {
-  constructor() {
-    this.selectedRepositoryName = '';
-    this.repositories = [];
-    this.repoList = document.getElementById('repoList');
-    this.searchDiv = document.getElementById('searchDiv');
-    this.searchInput = document.getElementById('searchInput');
-    this.repoName = document.getElementById('repoName');
-    this.fetchReposBtn = document.getElementById('fetchReposBtn');
-  }
-
-  setupEventListeners() {
-    this.fetchReposBtn.addEventListener('click', () => this.toggleGithubRepositories());
-    this.searchInput.addEventListener('keyup', () => this.filterRepositories());
-  }
-
-  async fetchGithubRepositories() {
-    try {
-      const response = await axios.post(API_ENDPOINT, { request: 'github_repos' });
-      this.repositories = response.data;
-      this.populateRepositoriesDropdown();
-      this.setSelectedRepository(this.repositories[0].name);
-    } catch (error) {
-      console.error(error);
-      // Show error to user
+  class GithubRepoManager {
+    constructor() {
+      this.selectedRepositoryName = '';
+      this.repositories = [];
+      this.repoList = document.getElementById('repoList');
+      this.searchDiv = document.getElementById('searchDiv');
+      this.searchInput = document.getElementById('searchInput');
+      this.repoName = document.getElementById('repoName');
+      this.fetchReposBtn = document.getElementById('fetchReposBtn');
+      this.setupEventListeners();
+      this.fetchGithubRepositories();
     }
-  }
 
-  toggleGithubRepositories() {
-  console.log('toggleGithubRepositories called');
-    if (this.repoList.classList.contains('hidden')) {
-      this.showRepos();
-      this.searchDiv.style.display = "flex";
-    } else {
-      this.hideRepos();
+    setupEventListeners() {
+      this.fetchReposBtn.addEventListener('click', () => this.toggleGithubRepositories());
+      this.searchInput.addEventListener('keyup', () => this.filterRepositories());
+    }
+
+    async fetchGithubRepositories() {
+      try {
+        const response = await axios.post(API_ENDPOINT, { request: 'github_repos' });
+        this.repositories = response.data;
+        this.populateRepositoriesDropdown();
+        this.setSelectedRepository(this.repositories[0].name);
+      } catch (error) {
+        console.error(error);
+        // Show error to user
+      }
+    }
+
+    toggleGithubRepositories() {
+      if (this.repoList.classList.contains('hidden')) {
+        this.showRepos();
+        this.searchDiv.style.display = "flex";
+      } else {
+        this.hideRepos();
+        this.searchDiv.style.display = "none";
+      }
+    }
+
+    showRepos() {
+      this.repoList.classList.remove('hidden');
+    }
+
+    hideRepos() {
+      this.repoList.classList.add('hidden');
       this.searchDiv.style.display = "none";
     }
-  }
 
-  showRepos() {
-    this.repoList.classList.remove('hidden');
-  }
+    populateRepositoriesDropdown() {
+      this.repoList.innerHTML = '';
+      this.repositories.forEach(repository => {
+        const repoElement = this.createRepoElement(repository.name);
+        this.repoList.appendChild(repoElement);
+      });
+    }
 
-  hideRepos() {
-    this.repoList.classList.add('hidden');
-    this.searchDiv.style.display = "none";
-  }
+    createRepoElement(repositoryName) {
+      const repoElement = document.createElement('li');
+      repoElement.textContent = repositoryName;
+      repoElement.addEventListener('click', () => this.setSelectedRepository(repositoryName));
+      return repoElement;
+    }
 
-  populateRepositoriesDropdown() {
-    this.repoList.innerHTML = '';
-    this.repositories.forEach(repository => {
-      const repoElement = this.createRepoElement(repository.name);
-      this.repoList.appendChild(repoElement);
-    });
-  }
+    setSelectedRepository(repositoryName) {
+      this.selectedRepositoryName = repositoryName;
+      this.repoName.textContent = repositoryName;
+      this.hideRepos();
+    }
 
-  createRepoElement(repositoryName) {
-    const repoElement = document.createElement('li');
-    repoElement.textContent = repositoryName;
-    repoElement.addEventListener('click', () => this.setSelectedRepository(repositoryName));
-    return repoElement;
-  }
-
-  setSelectedRepository(repositoryName) {
-    this.selectedRepositoryName = repositoryName;
-    this.repoName.textContent = repositoryName;
-    this.hideRepos();
-  }
-
-  filterRepositories() {
-    const filter = this.searchInput.value.toUpperCase();
-    const repoItems = this.repoList.getElementsByTagName('li');
-    for (let i = 0; i < repoItems.length; i++) {
-      let txtValue = repoItems[i].textContent || repoItems[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        repoItems[i].style.display = "";
-      } else {
-        repoItems[i].style.display = "none";
+    filterRepositories() {
+      const filter = this.searchInput.value.toUpperCase();
+      const repoItems = this.repoList.getElementsByTagName('li');
+      for (let i = 0; i < repoItems.length; i++) {
+        let txtValue = repoItems[i].textContent || repoItems[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          repoItems[i].style.display = "";
+        } else {
+          repoItems[i].style.display = "none";
+        }
       }
     }
   }
-}
 
-let githubRepoManager; // Declare it globally
-
-window.onload = () => {
-  githubRepoManager = new GithubRepoManager(); // Assign the instance to the global variable
-  githubRepoManager.setupEventListeners();
-  githubRepoManager.fetchGithubRepositories();
-
-  // Add event listener to the dropdown icon
-  document.getElementById('dropdownIcon').addEventListener('click', () => {
-    githubRepoManager.fetchGithubRepositories();
-  });
+  window.githubRepoManager = new GithubRepoManager();
 };
