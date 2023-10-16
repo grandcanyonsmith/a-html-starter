d5a28f0c8b4df4b4b25d9101ad9b5da92a2eb9d3
Canyon Smith <canyonfsmith@gmail.com>
2023-10-16 14:20:49 -0600

    Update RunTestsNew.js

--- RunTestsNew.js
+++ RunTestsNew.js
@@ -1,5 +1,7 @@
-const API_ENDPOINT = "https://nyk43gzspnm7wfhwqrc4uaprya0ecdap.lambda-url.us-west-2.on.aws/";
-const SUBMIT_REQUEST_API_ENDPOINT = 'https://uslbd6l6ssolgomdrcdhnqa5me0rnsee.lambda-url.us-west-2.on.aws/'
+const API_ENDPOINT =
+  "https://nyk43gzspnm7wfhwqrc4uaprya0ecdap.lambda-url.us-west-2.on.aws/";
+const SUBMIT_REQUEST_API_ENDPOINT =
+  "https://uslbd6l6ssolgomdrcdhnqa5me0rnsee.lambda-url.us-west-2.on.aws/";
 const HIDDEN_CLASS = "hidden";
 
 const SELECTORS = {
@@ -17,7 +19,8 @@ const SELECTORS = {
 };
 
 const URLS = {
-  runCode: "https://xhy5at2dbpxeys62rsx4f6lfay0yigqt.lambda-url.us-west-2.on.aws/"
+  runCode:
+    "https://xhy5at2dbpxeys62rsx4f6lfay0yigqt.lambda-url.us-west-2.on.aws/"
 };
 
 class RepositoryManager {
@@ -56,12 +59,16 @@ class RepositoryManager {
       }
     } catch (error) {
       console.error(`Error fetching repository contents: ${error}`);
-      alert("An error occurred while fetching the repository contents. Please try again.");
+      alert(
+        "An error occurred while fetching the repository contents. Please try again."
+      );
     }
   }
 
   toggleElementsVisibility(elements, className) {
-    elements.forEach(id => document.getElementById(id).classList.toggle(className));
+    elements.forEach((id) =>
+      document.getElementById(id).classList.toggle(className)
+    );
   }
 
   populateFilesDropdown(contents) {
@@ -72,7 +79,9 @@ class RepositoryManager {
       if (content.type === "file") {
         filesDropdown.appendChild(this.createFileElement(displayName));
         if (index === 0) {
-          document.getElementById(SELECTORS.fileTitle).textContent = displayName;
+          document.getElementById(
+            SELECTORS.fileTitle
+          ).textContent = displayName;
         }
       }
       if (content.type === "dir") {
@@ -90,13 +99,17 @@ class RepositoryManager {
     fileElement.href = "#";
     fileElement.className = "block px-4 py-2 text-sm hover:bg-gray-200";
     fileElement.textContent = fileName;
-    fileElement.addEventListener("click", () => this.changeTitleAndHide(fileName, filePath));
+    fileElement.addEventListener("click", () =>
+      this.changeTitleAndHide(fileName, filePath)
+    );
     return fileElement;
   }
 
   createDirectoryElement(directory) {
     const directoryElement = document.createElement("div");
-    const directoryContentElement = this.createDirectoryContentElement(directory.contents);
+    const directoryContentElement = this.createDirectoryContentElement(
+      directory.contents
+    );
     const spanElement = this.createSpanElement(directory.path);
 
     directoryElement.appendChild(spanElement);
@@ -107,14 +120,22 @@ class RepositoryManager {
 
   createDirectoryContentElement(contents) {
     const directoryContentElement = document.createElement("div");
-    directoryContentElement.className = "border-l-2 border-gray-200 pl-2 hidden";
+    directoryContentElement.className =
+      "border-l-2 border-gray-200 pl-2 hidden";
 
-    contents.forEach(content => {
+    contents.forEach((content) => {
       if (content.type === "file") {
-        directoryContentElement.appendChild(this.createFileElement(this.extractFileNameFromPath(content.path), content.path));
+        directoryContentElement.appendChild(
+          this.createFileElement(
+            this.extractFileNameFromPath(content.path),
+            content.path
+          )
+        );
       }
       if (content.type === "dir") {
-        directoryContentElement.appendChild(this.createDirectoryElement(content));
+        directoryContentElement.appendChild(
+          this.createDirectoryElement(content)
+        );
       }
     });
 
@@ -123,16 +144,20 @@ class RepositoryManager {
 
   createSpanElement(path) {
     const spanElement = document.createElement("span");
-    spanElement.className = "flex items-center px-4 py-2 text-sm cursor-pointer select-none";
+    spanElement.className =
+      "flex items-center px-4 py-2 text-sm cursor-pointer select-none";
 
     const fileNameElement = document.createElement("span");
     fileNameElement.textContent = this.extractFileNameFromPath(path);
-    fileNameElement.addEventListener("click", () => this.changeTitleAndHide(this.extractFileNameFromPath(path)));
+    fileNameElement.addEventListener("click", () =>
+      this.changeTitleAndHide(this.extractFileNameFromPath(path))
+    );
 
     spanElement.appendChild(fileNameElement);
-    spanElement.innerHTML += ' <i data-feather="chevron-down" class="ml-1 w-4 h-4 transform"></i>';
+    spanElement.innerHTML +=
+      ' <i data-feather="chevron-down" class="ml-1 w-4 h-4 transform"></i>';
 
-    spanElement.addEventListener("click", event => {
+    spanElement.addEventListener("click", (event) => {
       event.currentTarget.nextElementSibling.classList.toggle("hidden");
       event.currentTarget.children[1].classList.toggle("rotate-180");
     });
@@ -142,29 +167,46 @@ class RepositoryManager {
 
   async changeTitleAndHide(title, filePath) {
     document.getElementById(SELECTORS.fileTitle).textContent = title;
-    this.toggleElementsVisibility([SELECTORS.fileDropdown, SELECTORS.searchBar, SELECTORS.createFileBtn], HIDDEN_CLASS);
-    document.getElementById(SELECTORS.filePath).textContent = filePath;
+    this.toggleElementsVisibility(
+      [SELECTORS.fileDropdown, SELECTORS.searchBar, SELECTORS.createFileBtn],
+      HIDDEN_CLASS
+    );
+    document.getElementById("filePath").textContent = filePath;
 
     const fileContents = await this.fetchFileContents();
-    document.getElementById("codeBox").innerHTML = Prism.highlight(fileContents.file_content, Prism.languages.python, "python");
+    document.getElementById("codeBox").innerHTML = Prism.highlight(
+      fileContents.file_content,
+      Prism.languages.python,
+      "python"
+    );
   }
 
   searchFilesAndFolders(query) {
-    const filteredContents = this.filesAndFolders.filter(content => content.path.includes(query));
+    const filteredContents = this.filesAndFolders.filter((content) =>
+      content.path.includes(query)
+    );
     this.populateFilesDropdown(filteredContents);
   }
 
   openCreateNewFileModal() {
-    document.getElementById(SELECTORS.createNewFileModalWindow).classList.remove(HIDDEN_CLASS);
+    document
+      .getElementById(SELECTORS.createNewFileModalWindow)
+      .classList.remove(HIDDEN_CLASS);
   }
 
   closeCreateNewFileModal() {
-    document.getElementById(SELECTORS.createNewFileModalWindow).classList.add(HIDDEN_CLASS);
+    document
+      .getElementById(SELECTORS.createNewFileModalWindow)
+      .classList.add(HIDDEN_CLASS);
   }
 
   async submitFile() {
-    const newFileNameInput = document.getElementById(SELECTORS.newFileNameInput);
-    const newFileContentsInput = document.getElementById(SELECTORS.newFileContentsInput);
+    const newFileNameInput = document.getElementById(
+      SELECTORS.newFileNameInput
+    );
+    const newFileContentsInput = document.getElementById(
+      SELECTORS.newFileContentsInput
+    );
     if (!newFileNameInput.value || !newFileContentsInput.value) {
       alert("Please fill out all fields.");
       return;
@@ -194,8 +236,8 @@ class RepositoryManager {
 }
 
 async function submit() {
-  const codeBox = document.getElementById('codeBox');
-  const requestInput = document.getElementById('requestInput');
+  const codeBox = document.getElementById("codeBox");
+  const requestInput = document.getElementById("requestInput");
   const fileName = document.getElementById(SELECTORS.filePath).textContent;
   const repoName = "a-canyon-yb-tests";
   const code = codeBox.textContent;
@@ -208,8 +250,8 @@ async function submit() {
     repoName: repoName
   };
 
-  const submitBtn = document.getElementById('submitBtn');
-  submitBtn.innerHTML = 'Loading...'; // Change button text to 'Loading...'
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.innerHTML = "Loading..."; // Change button text to 'Loading...'
 
   try {
     const response = await axios.post(SUBMIT_REQUEST_API_ENDPOINT, data);
@@ -219,7 +261,7 @@ async function submit() {
       Prism.highlightAll();
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   } finally {
     submitBtn.innerHTML = '<i data-feather="send"></i>'; // Change button text back to the send icon
     feather.replace(); // Re-initialize Feather Icons
@@ -235,12 +277,12 @@ function escapeHtml(unsafe) {
 }
 
 async function runTests() {
-  const testResult = document.getElementById('testResult');
-  const codeBox = document.getElementById('codeBox');
+  const testResult = document.getElementById("testResult");
+  const codeBox = document.getElementById("codeBox");
   const filePath = document.getElementById(SELECTORS.filePath).textContent;
 
-  const runTestsBtn = document.getElementById('runTestsBtn');
-  runTestsBtn.innerHTML = 'Running...'; // Change button text to 'Running...'
+  const runTestsBtn = document.getElementById("runTestsBtn");
+  runTestsBtn.innerHTML = "Running..."; // Change button text to 'Running...'
 
   testResult.innerHTML = `<pre>Running Tests in EC2 instance</pre>`;
 
@@ -253,19 +295,41 @@ async function runTests() {
     let error = escapeHtml(response.data.error);
     testResult.innerHTML = `<pre>Output:\n ${output} \n Error:\n ${error}</pre>`;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   } finally {
     runTestsBtn.innerHTML = '<i data-feather="play"></i>'; // Change button text back to the play icon
     feather.replace(); // Re-initialize Feather Icons
   }
 }
 
-let repoManager;
+async function saveCode() {
+  const repoName = "a-canyon-yb-tests"; // replace with actual repo name
+  const fileName = document.getElementById(SELECTORS.filePath).textContent;
+  const fileContents = document.getElementById("codeBox").textContent; // replace with actual file contents
+  const branchName = "master"; // replace with actual branch name if needed
 
+  const data = {
+    repo_name: repoName,
+    file_contents: fileContents,
+    file_name: fileName,
+    request: "update",
+    branch_name: branchName
+  };
+  console.log(data, "data");
+  try {
+    const response = await axios.post(API_ENDPOINT, data);
+    console.log(response.data);
+  } catch (error) {
+    console.error(`Error saving file: ${error}`);
+  }
+}
+let repoManager;
 window.onload = () => {
   repoManager = new RepositoryManager();
   repoManager.init();
-  document.getElementById('submitBtn').addEventListener('click', submit);
-  document.getElementById('runTestsBtn').addEventListener('click', runTests);
+  document.getElementById("submitBtn").addEventListener("click", submit);
+  document.getElementById("saveBtn").classList.remove("hidden");
+  document.getElementById("saveBtn").addEventListener("click", saveCode);
+  document.getElementById("runTestsBtn").addEventListener("click", runTests);
   feather.replace(); // Add this line
-  };
+};
