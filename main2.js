c70bd0b83b83d8e6569a8ad1d65f98333c48ebcb
Canyon Smith <canyonfsmith@gmail.com>
2023-11-23 12:38:44 -0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -138,7 +138,7 @@ const ELEMENT_IDS = [
   
   const URLS = {
     runCode: "https://xhy5at2dbpxeys62rsx4f6lfay0yigqt.lambda-url.us-west-2.on.aws/",
-    submitRequest: "https://43ofpuyi3vyyxfjpdgwz7pvvyq0xulcs.lambda-url.us-west-2.on.aws/",
+    submitRequest: "https://kvqpfgxn2jz5pyh4wz7thbmhay0hqcvh.lambda-url.us-west-2.on.aws/",
     getRepos: "https://flask-hello-world2-three.vercel.app/github_repos_list",
     getFiles: "https://flask-hello-world2-git-main-grandcanyonsmith.vercel.app/github_repos/",
     update: "https://flask-hello-world2-three.vercel.app/update",
