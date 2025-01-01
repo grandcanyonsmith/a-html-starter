21f7fc425f0dd18fc493c8594fc23e97b7509dbd
Canyon Smith <canyonfsmith@gmail.com>
2024-12-31 17:53:31 -0700

    Create colorData.js

---
+++ colorData.js
@@ -0,0 +1,174 @@
+// colorData.js
+// We export a single array of objects.
+// The first 3 objects are your “top row” colors.
+// The next 15 are the rest.
+
+export default [
+  // ========== TOP 3 COLORS ==========
+  {
+    name: "Future Green",
+    color: "#b2ff00",
+    type: "PRIMARY COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/0/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+  {
+    name: "Black",
+    color: "#000000",
+    type: "PRIMARY BACKGROUND COLOR",
+    textColor: "#ffffff",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/0/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+  {
+    name: "White",
+    color: "#ffffff",
+    type: "SECONDARY BACKGROUND COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/0/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+
+  // ========== 15 MORE ITEMS ==========
+  {
+    name: "Future Green",
+    color: "#b2ff00",
+    type: "PRIMARY COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+  {
+    name: "Black",
+    color: "#fbfbfb",
+    type: "PRIMARY BACKGROUND COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+  {
+    name: "White",
+    color: "#131313",
+    type: "SECONDARY BACKGROUND COLOR",
+    textColor: "#ffffff",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50",
+      pantone: "902C"
+    }
+  },
+  {
+    name: "Sky",
+    color: "#4dadf9",
+    type: "SECONDARY COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50"
+    }
+  },
+  {
+    name: "Violet",
+    color: "#7c5de2",
+    type: "SECONDARY COLOR",
+    textColor: "#ffffff",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50"
+    }
+  },
+  {
+    name: "Gray",
+    color: "#b1b1b1",
+    type: "SECONDARY COLOR",
+    textColor: "#000000",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "#C4FC50"
+    }
+  },
+  {
+    name: "Dark Gray",
+    color: "#131313",
+    type: "",
+    textColor: "#ffffff",
+    specs: {
+      rgb: "176/255/0",
+      cmyk: "30/01/100/0C4FC50",
+      hex: "C4FC50"
+    }
+  },
+  {
+    name: "Sky",
+    color: "#fffb54",
+    type: "",
+    textColor: "#000000"
+  },
+  {
+    name: "Light Pink",
+    color: "#f1beeb",
+    type: "",
+    textColor: "#000000"
+  },
+  {
+    name: "Sky",
+    color: "#6acd50",
+    type: "",
+    textColor: "#000000"
+  },
+  {
+    name: "Sky",
+    color: "#7697e6",
+    type: "",
+    textColor: "#ffffff"
+  },
+  {
+    name: "Orange",
+    color: "#f2952f",
+    type: "",
+    textColor: "#000000"
+  },
+  {
+    name: "Light Pink",
+    color: "#e8555f",
+    type: "",
+    textColor: "#ffffff"
+  },
+  {
+    name: "Dark Green",
+    color: "#40714e",
+    type: "",
+    textColor: "#ffffff"
+  },
+  {
+    name: "Brown",
+    color: "#a97f55",
+    type: "",
+    textColor: "#ffffff"
+  }
+];
