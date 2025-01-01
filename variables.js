dac6713b5c8585b05e4b86d9866dba212b2b48b0
Canyon Smith <canyonfsmith@gmail.com>
2024-12-31 18:19:33 -0700

    Updating file content via Lambda

--- variables.js
+++ variables.js
@@ -3,32 +3,32 @@
  *******************************************************/
 
 // -- TOP THREE COLORS --
-const export primaryColorName = "Home Depot Orange";
-const export primaryColorHexCode = "#F96302";                // The signature Home Depot orange
-const export primaryColorType = "PRIMARY COLOR";
-const export primaryColorTextColor = "#FFFFFF";              // White text for contrast
-const export primaryColorRgb = "249,99,2";                   // Approx. R/G/B
-const export primaryColorCmyk = "0,64,100,0";                // Approx. C/M/Y/K
-const export primaryColorHexSpec = "#F96302";
-const export primaryColorPantone = "PANTONE 172C";           // Approx. Pantone
+export const primaryColorName = "Home Depot Orange";
+export const primaryColorHexCode = "#F96302";                // The signature Home Depot orange
+export const primaryColorType = "PRIMARY COLOR";
+export const primaryColorTextColor = "#FFFFFF";              // White text for contrast
+export const primaryColorRgb = "249,99,2";                   // Approx. R/G/B
+export const primaryColorCmyk = "0,64,100,0";                // Approx. C/M/Y/K
+export const primaryColorHexSpec = "#F96302";
+export const primaryColorPantone = "PANTONE 172C";           // Approx. Pantone
 
-const export primaryBackgroundColorName = "White";
-const export primaryBackgroundColorHexCode = "#FFFFFF";
-const export primaryBackgroundColorType = "PRIMARY BACKGROUND COLOR";
-const export primaryBackgroundColorTextColor = "#000000";    // Black text for contrast
-const export primaryBackgroundColorRgb = "255,255,255";
-const export primaryBackgroundColorCmyk = "0,0,0,0";
-const export primaryBackgroundColorHexSpec = "#FFFFFF";
-const export primaryBackgroundColorPantone = "N/A";
+export const primaryBackgroundColorName = "White";
+export const primaryBackgroundColorHexCode = "#FFFFFF";
+export const primaryBackgroundColorType = "PRIMARY BACKGROUND COLOR";
+export const primaryBackgroundColorTextColor = "#000000";    // Black text for contrast
+export const primaryBackgroundColorRgb = "255,255,255";
+export const primaryBackgroundColorCmyk = "0,0,0,0";
+export const primaryBackgroundColorHexSpec = "#FFFFFF";
+export const primaryBackgroundColorPantone = "N/A";
 
-const export secondaryBackgroundColorName = "Dark Gray";
-const export secondaryBackgroundColorHexCode = "#4D4F53";
-const export secondaryBackgroundColorType = "SECONDARY BACKGROUND COLOR";
-const export secondaryBackgroundColorTextColor = "#FFFFFF";
-const export secondaryBackgroundColorRgb = "77,79,83";
-const export secondaryBackgroundColorCmyk = "7,5,0,68";
-const export secondaryBackgroundColorHexSpec = "#4D4F53";
-const export secondaryBackgroundColorPantone = "N/A";
+export const secondaryBackgroundColorName = "Dark Gray";
+export const secondaryBackgroundColorHexCode = "#4D4F53";
+export const secondaryBackgroundColorType = "SECONDARY BACKGROUND COLOR";
+export const secondaryBackgroundColorTextColor = "#FFFFFF";
+export const secondaryBackgroundColorRgb = "77,79,83";
+export const secondaryBackgroundColorCmyk = "7,5,0,68";
+export const secondaryBackgroundColorHexSpec = "#4D4F53";
+export const secondaryBackgroundColorPantone = "N/A";
 
 
 /*******************************************************
@@ -38,143 +38,143 @@ const export secondaryBackgroundColorPantone = "N/A";
 /**
  * 1
  */
-const export primaryColor2Name = "Home Depot Orange (Light)";
-const export primaryColor2HexCode = "#FC8540";
-const export primaryColor2Type = "PRIMARY COLOR";
-const export primaryColor2TextColor = "#000000";             
-const export primaryColor2Rgb = "252,133,64";
-const export primaryColor2Cmyk = "0,52,78,1"; 
-const export primaryColor2HexSpec = "#FC8540";
-const export primaryColor2Pantone = "N/A";
+export const primaryColor2Name = "Home Depot Orange (Light)";
+export const primaryColor2HexCode = "#FC8540";
+export const primaryColor2Type = "PRIMARY COLOR";
+export const primaryColor2TextColor = "#000000";             
+export const primaryColor2Rgb = "252,133,64";
+export const primaryColor2Cmyk = "0,52,78,1"; 
+export const primaryColor2HexSpec = "#FC8540";
+export const primaryColor2Pantone = "N/A";
 
 /**
  * 2
  */
-const export primaryBackgroundColor2Name = "Light Gray";
-const export primaryBackgroundColor2HexCode = "#EDEDED";
-const export primaryBackgroundColor2Type = "PRIMARY BACKGROUND COLOR";
-const export primaryBackgroundColor2TextColor = "#000000";
-const export primaryBackgroundColor2Rgb = "237,237,237";
-const export primaryBackgroundColor2Cmyk = "0,0,0,7";
-const export primaryBackgroundColor2HexSpec = "#EDEDED";
-const export primaryBackgroundColor2Pantone = "N/A";
+export const primaryBackgroundColor2Name = "Light Gray";
+export const primaryBackgroundColor2HexCode = "#EDEDED";
+export const primaryBackgroundColor2Type = "PRIMARY BACKGROUND COLOR";
+export const primaryBackgroundColor2TextColor = "#000000";
+export const primaryBackgroundColor2Rgb = "237,237,237";
+export const primaryBackgroundColor2Cmyk = "0,0,0,7";
+export const primaryBackgroundColor2HexSpec = "#EDEDED";
+export const primaryBackgroundColor2Pantone = "N/A";
 
 /**
  * 3
  */
-const export secondaryBackgroundColor2Name = "Home Depot Gray";
-const export secondaryBackgroundColor2HexCode = "#767676";
-const export secondaryBackgroundColor2Type = "SECONDARY BACKGROUND COLOR";
-const export secondaryBackgroundColor2TextColor = "#FFFFFF";
-const export secondaryBackgroundColor2Rgb = "118,118,118";
-const export secondaryBackgroundColor2Cmyk = "0,0,0,54";
-const export secondaryBackgroundColor2HexSpec = "#767676";
-const export secondaryBackgroundColor2Pantone = "N/A";
+export const secondaryBackgroundColor2Name = "Home Depot Gray";
+export const secondaryBackgroundColor2HexCode = "#767676";
+export const secondaryBackgroundColor2Type = "SECONDARY BACKGROUND COLOR";
+export const secondaryBackgroundColor2TextColor = "#FFFFFF";
+export const secondaryBackgroundColor2Rgb = "118,118,118";
+export const secondaryBackgroundColor2Cmyk = "0,0,0,54";
+export const secondaryBackgroundColor2HexSpec = "#767676";
+export const secondaryBackgroundColor2Pantone = "N/A";
 
 /**
  * 4
  */
-const export secondaryColor1Name = "Home Depot Tan";
-const export secondaryColor1HexCode = "#F3E2C7";
-const export secondaryColor1Type = "SECONDARY COLOR";
-const export secondaryColor1TextColor = "#000000";
-const export secondaryColor1Rgb = "243,226,199";
-const export secondaryColor1Cmyk = "2,6,17,5";
-const export secondaryColor1HexSpec = "#F3E2C7";
+export const secondaryColor1Name = "Home Depot Tan";
+export const secondaryColor1HexCode = "#F3E2C7";
+export const secondaryColor1Type = "SECONDARY COLOR";
+export const secondaryColor1TextColor = "#000000";
+export const secondaryColor1Rgb = "243,226,199";
+export const secondaryColor1Cmyk = "2,6,17,5";
+export const secondaryColor1HexSpec = "#F3E2C7";
 
 /**
  * 5
  */
-const export secondaryColor2Name = "Home Depot Dark Brown";
-const export secondaryColor2HexCode = "#9C6A27";
-const export secondaryColor2Type = "SECONDARY COLOR";
-const export secondaryColor2TextColor = "#FFFFFF";
-const export secondaryColor2Rgb = "156,106,39";
-const export secondaryColor2Cmyk = "0,32,75,39";
-const export secondaryColor2HexSpec = "#9C6A27";
+export const secondaryColor2Name = "Home Depot Dark Brown";
+export const secondaryColor2HexCode = "#9C6A27";
+export const secondaryColor2Type = "SECONDARY COLOR";
+export const secondaryColor2TextColor = "#FFFFFF";
+export const secondaryColor2Rgb = "156,106,39";
+export const secondaryColor2Cmyk = "0,32,75,39";
+export const secondaryColor2HexSpec = "#9C6A27";
 
 /**
  * 6
  */
-const export secondaryColor3Name = "Home Depot Mid Gray";
-const export secondaryColor3HexCode = "#8B8D8E";
-const export secondaryColor3Type = "SECONDARY COLOR";
-const export secondaryColor3TextColor = "#000000";
-const export secondaryColor3Rgb = "139,141,142";
-const export secondaryColor3Cmyk = "2,1,0,44";
-const export secondaryColor3HexSpec = "#8B8D8E";
+export const secondaryColor3Name = "Home Depot Mid Gray";
+export const secondaryColor3HexCode = "#8B8D8E";
+export const secondaryColor3Type = "SECONDARY COLOR";
+export const secondaryColor3TextColor = "#000000";
+export const secondaryColor3Rgb = "139,141,142";
+export const secondaryColor3Cmyk = "2,1,0,44";
+export const secondaryColor3HexSpec = "#8B8D8E";
 
 /**
  * 7
  */
-const export miscColor1Name = "Black";
-const export miscColor1HexCode = "#000000";
-const export miscColor1Type = "";
-const export miscColor1TextColor = "#FFFFFF";
-const export miscColor1Rgb = "0,0,0";
-const export miscColor1Cmyk = "0,0,0,100";
-const export miscColor1HexSpec = "#000000";
+export const miscColor1Name = "Black";
+export const miscColor1HexCode = "#000000";
+export const miscColor1Type = "";
+export const miscColor1TextColor = "#FFFFFF";
+export const miscColor1Rgb = "0,0,0";
+export const miscColor1Cmyk = "0,0,0,100";
+export const miscColor1HexSpec = "#000000";
 
 /**
  * 8
  */
-const export miscColor2Name = "Home Depot Off-White";
-const export miscColor2HexCode = "#F7F4F1";
-const export miscColor2Type = "";
-const export miscColor2TextColor = "#000000";
+export const miscColor2Name = "Home Depot Off-White";
+export const miscColor2HexCode = "#F7F4F1";
+export const miscColor2Type = "";
+export const miscColor2TextColor = "#000000";
 
 /**
  * 9
  */
-const export miscColor3Name = "Home Depot Silver";
-const export miscColor3HexCode = "#E0E1E2";
-const export miscColor3Type = "";
-const export miscColor3TextColor = "#000000";
+export const miscColor3Name = "Home Depot Silver";
+export const miscColor3HexCode = "#E0E1E2";
+export const miscColor3Type = "";
+export const miscColor3TextColor = "#000000";
 
 /**
  * 10
  */
-const export miscColor4Name = "Home Depot Green Accent";
-const export miscColor4HexCode = "#2D5A27";
-const export miscColor4Type = "";
-const export miscColor4TextColor = "#FFFFFF";
+export const miscColor4Name = "Home Depot Green Accent";
+export const miscColor4HexCode = "#2D5A27";
+export const miscColor4Type = "";
+export const miscColor4TextColor = "#FFFFFF";
 
 /**
  * 11
  */
-const export miscColor5Name = "Deep Gray";
-const export miscColor5HexCode = "#333333";
-const export miscColor5Type = "";
-const export miscColor5TextColor = "#FFFFFF";
+export const miscColor5Name = "Deep Gray";
+export const miscColor5HexCode = "#333333";
+export const miscColor5Type = "";
+export const miscColor5TextColor = "#FFFFFF";
 
 /**
  * 12
  */
-const export miscColor6Name = "Home Depot Light Tan";
-const export miscColor6HexCode = "#DBCFB9";
-const export miscColor6Type = "";
-const export miscColor6TextColor = "#000000";
+export const miscColor6Name = "Home Depot Light Tan";
+export const miscColor6HexCode = "#DBCFB9";
+export const miscColor6Type = "";
+export const miscColor6TextColor = "#000000";
 
 /**
  * 13
  */
-const export miscColor7Name = "Home Depot Red Alert";
-const export miscColor7HexCode = "#D42D2D";
-const export miscColor7Type = "";
-const export miscColor7TextColor = "#FFFFFF";
+export const miscColor7Name = "Home Depot Red Alert";
+export const miscColor7HexCode = "#D42D2D";
+export const miscColor7Type = "";
+export const miscColor7TextColor = "#FFFFFF";
 
 /**
  * 14
  */
-const export miscColor8Name = "Dark Brown";
-const export miscColor8HexCode = "#6B4D3E";
-const export miscColor8Type = "";
-const export miscColor8TextColor = "#FFFFFF";
+export const miscColor8Name = "Dark Brown";
+export const miscColor8HexCode = "#6B4D3E";
+export const miscColor8Type = "";
+export const miscColor8TextColor = "#FFFFFF";
 
 /**
  * 15
  */
-const export miscColor9Name = "Very Dark Gray";
-const export miscColor9HexCode = "#1D1D1B";
-const export miscColor9Type = "";
-const export miscColor9TextColor = "#FFFFFF";
+export const miscColor9Name = "Very Dark Gray";
+export const miscColor9HexCode = "#1D1D1B";
+export const miscColor9Type = "";
+export const miscColor9TextColor = "#FFFFFF";
