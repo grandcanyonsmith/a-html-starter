10bd171d36316ac1f80a69c22135de77d77024d0
Canyon Smith <canyonfsmith@gmail.com>
2024-12-31 18:15:09 -0700

    Updating file content via Lambda

--- variables.js
+++ variables.js
@@ -1,129 +1,149 @@
-// variables.js
+/*******************************************************
+ * Chase Bank–Inspired Color Variables
+ *******************************************************/
 
 // -- TOP THREE COLORS --
-export const primaryColorName = "Future Green";
-export const primaryColorHexCode = "#b2ff00";
+export const primaryColorName = "Chase Blue";
+export const primaryColorHexCode = "#0072CE";
 export const primaryColorType = "PRIMARY COLOR";
-export const primaryColorTextColor = "#000000";
-export const primaryColorRgb = "176/255/0";
-export const primaryColorCmyk = "30/0/100/0C4FC50";
-export const primaryColorHexSpec = "#C4FC50";
-export const primaryColorPantone = "902C";
+export const primaryColorTextColor = "#FFFFFF";       // White text for contrast
+export const primaryColorRgb = "0,114,206";          // Approx. R/G/B
+export const primaryColorCmyk = "100,45,0,5";        // Approx. C/M/Y/K
+export const primaryColorHexSpec = "#0072CE";
+export const primaryColorPantone = "PANTONE 285C";   // Approx. Pantone match
 
-export const primaryBackgroundColorName = "Black";
-export const primaryBackgroundColorHexCode = "#000000";
+export const primaryBackgroundColorName = "White";
+export const primaryBackgroundColorHexCode = "#FFFFFF";
 export const primaryBackgroundColorType = "PRIMARY BACKGROUND COLOR";
-export const primaryBackgroundColorTextColor = "#ffffff";
-export const primaryBackgroundColorRgb = "176/255/0";
-export const primaryBackgroundColorCmyk = "30/0/100/0C4FC50";
-export const primaryBackgroundColorHexSpec = "#C4FC50";
-export const primaryBackgroundColorPantone = "902C";
+export const primaryBackgroundColorTextColor = "#000000";  // Black text for contrast
+export const primaryBackgroundColorRgb = "255,255,255";
+export const primaryBackgroundColorCmyk = "0,0,0,0";
+export const primaryBackgroundColorHexSpec = "#FFFFFF";
+export const primaryBackgroundColorPantone = "N/A";
 
-export const secondaryBackgroundColorName = "White";
-export const secondaryBackgroundColorHexCode = "#ffffff";
+export const secondaryBackgroundColorName = "Dark Gray";
+export const secondaryBackgroundColorHexCode = "#4D4F53";
 export const secondaryBackgroundColorType = "SECONDARY BACKGROUND COLOR";
-export const secondaryBackgroundColorTextColor = "#000000";
-export const secondaryBackgroundColorRgb = "176/255/0";
-export const secondaryBackgroundColorCmyk = "30/0/100/0C4FC50";
-export const secondaryBackgroundColorHexSpec = "#C4FC50";
-export const secondaryBackgroundColorPantone = "902C";
+export const secondaryBackgroundColorTextColor = "#FFFFFF"; // White text for contrast
+export const secondaryBackgroundColorRgb = "77,79,83";
+export const secondaryBackgroundColorCmyk = "7,5,0,68";
+export const secondaryBackgroundColorHexSpec = "#4D4F53";
+export const secondaryBackgroundColorPantone = "N/A";
 
-// -- COLOR DATA (15 items) --
-export const primaryColor2Name = "Future Green";
-export const primaryColor2HexCode = "#b2ff00";
+/*******************************************************
+ * COLOR DATA (15 items)
+ *******************************************************/
+
+// 1
+export const primaryColor2Name = "Chase Blue (Light)";
+export const primaryColor2HexCode = "#6AADDA";
 export const primaryColor2Type = "PRIMARY COLOR";
 export const primaryColor2TextColor = "#000000";
-export const primaryColor2Rgb = "176/255/0";
-export const primaryColor2Cmyk = "30/01/100/0C4FC50";
-export const primaryColor2HexSpec = "#C4FC50";
-export const primaryColor2Pantone = "902C";
+export const primaryColor2Rgb = "106,173,218";
+export const primaryColor2Cmyk = "51,21,0,0"; 
+export const primaryColor2HexSpec = "#6AADDA";
+export const primaryColor2Pantone = "N/A";
 
-export const primaryBackgroundColor2Name = "Black";
-export const primaryBackgroundColor2HexCode = "#fbfbfb";
+// 2
+export const primaryBackgroundColor2Name = "Chase Gray (Light)";
+export const primaryBackgroundColor2HexCode = "#B7BBC0";
 export const primaryBackgroundColor2Type = "PRIMARY BACKGROUND COLOR";
 export const primaryBackgroundColor2TextColor = "#000000";
-export const primaryBackgroundColor2Rgb = "176/255/0";
-export const primaryBackgroundColor2Cmyk = "30/01/100/0C4FC50";
-export const primaryBackgroundColor2HexSpec = "#C4FC50";
-export const primaryBackgroundColor2Pantone = "902C";
+export const primaryBackgroundColor2Rgb = "183,187,192";
+export const primaryBackgroundColor2Cmyk = "5,3,0,25";
+export const primaryBackgroundColor2HexSpec = "#B7BBC0";
+export const primaryBackgroundColor2Pantone = "N/A";
 
-export const secondaryBackgroundColor2Name = "White";
-export const secondaryBackgroundColor2HexCode = "#131313";
+// 3
+export const secondaryBackgroundColor2Name = "Chase Gray (Dark)";
+export const secondaryBackgroundColor2HexCode = "#2F3033";
 export const secondaryBackgroundColor2Type = "SECONDARY BACKGROUND COLOR";
-export const secondaryBackgroundColor2TextColor = "#ffffff";
-export const secondaryBackgroundColor2Rgb = "176/255/0";
-export const secondaryBackgroundColor2Cmyk = "30/01/100/0C4FC50";
-export const secondaryBackgroundColor2HexSpec = "#C4FC50";
-export const secondaryBackgroundColor2Pantone = "902C";
+export const secondaryBackgroundColor2TextColor = "#FFFFFF";
+export const secondaryBackgroundColor2Rgb = "47,48,51";
+export const secondaryBackgroundColor2Cmyk = "8,6,0,80";
+export const secondaryBackgroundColor2HexSpec = "#2F3033";
+export const secondaryBackgroundColor2Pantone = "N/A";
 
-export const secondaryColor1Name = "Sky";
-export const secondaryColor1HexCode = "#4dadf9";
+// 4
+export const secondaryColor1Name = "Chase Light Blue";
+export const secondaryColor1HexCode = "#99CDEE";
 export const secondaryColor1Type = "SECONDARY COLOR";
 export const secondaryColor1TextColor = "#000000";
-export const secondaryColor1Rgb = "176/255/0";
-export const secondaryColor1Cmyk = "30/01/100/0C4FC50";
-export const secondaryColor1HexSpec = "#C4FC50";
+export const secondaryColor1Rgb = "153,205,238";
+export const secondaryColor1Cmyk = "36,10,0,0";
+export const secondaryColor1HexSpec = "#99CDEE";
 
-export const secondaryColor2Name = "Violet";
-export const secondaryColor2HexCode = "#7c5de2";
+// 5
+export const secondaryColor2Name = "Chase Dark Blue";
+export const secondaryColor2HexCode = "#1C2D5B";
 export const secondaryColor2Type = "SECONDARY COLOR";
-export const secondaryColor2TextColor = "#ffffff";
-export const secondaryColor2Rgb = "176/255/0";
-export const secondaryColor2Cmyk = "30/01/100/0C4FC50";
-export const secondaryColor2HexSpec = "#C4FC50";
+export const secondaryColor2TextColor = "#FFFFFF";
+export const secondaryColor2Rgb = "28,45,91";
+export const secondaryColor2Cmyk = "81,66,0,64";
+export const secondaryColor2HexSpec = "#1C2D5B";
 
-export const secondaryColor3Name = "Gray";
-export const secondaryColor3HexCode = "#b1b1b1";
+// 6
+export const secondaryColor3Name = "Chase Mid Gray";
+export const secondaryColor3HexCode = "#8B8D8E";
 export const secondaryColor3Type = "SECONDARY COLOR";
 export const secondaryColor3TextColor = "#000000";
-export const secondaryColor3Rgb = "176/255/0";
-export const secondaryColor3Cmyk = "30/01/100/0C4FC50";
-export const secondaryColor3HexSpec = "#C4FC50";
+export const secondaryColor3Rgb = "139,141,142";
+export const secondaryColor3Cmyk = "2,1,0,44";
+export const secondaryColor3HexSpec = "#8B8D8E";
 
-export const miscColor1Name = "Dark Gray";
-export const miscColor1HexCode = "#131313";
+// 7
+export const miscColor1Name = "Chase Black";
+export const miscColor1HexCode = "#000000";
 export const miscColor1Type = "";
-export const miscColor1TextColor = "#ffffff";
-export const miscColor1Rgb = "176/255/0";
-export const miscColor1Cmyk = "30/01/100/0C4FC50";
-export const miscColor1HexSpec = "C4FC50";
+export const miscColor1TextColor = "#FFFFFF";
+export const miscColor1Rgb = "0,0,0";
+export const miscColor1Cmyk = "0,0,0,100";
+export const miscColor1HexSpec = "#000000";
 
-export const miscColor2Name = "Sky";
-export const miscColor2HexCode = "#fffb54";
+// 8
+export const miscColor2Name = "Chase Off-White";
+export const miscColor2HexCode = "#F3F2F1";
 export const miscColor2Type = "";
 export const miscColor2TextColor = "#000000";
 
-export const miscColor3Name = "Light Pink";
-export const miscColor3HexCode = "#f1beeb";
+// 9
+export const miscColor3Name = "Chase Silver";
+export const miscColor3HexCode = "#E0E1E2";
 export const miscColor3Type = "";
 export const miscColor3TextColor = "#000000";
 
-export const miscColor4Name = "Sky";
-export const miscColor4HexCode = "#6acd50";
+// 10
+export const miscColor4Name = "Chase Teal Accent";
+export const miscColor4HexCode = "#008E8E";
 export const miscColor4Type = "";
-export const miscColor4TextColor = "#000000";
+export const miscColor4TextColor = "#FFFFFF";
 
-export const miscColor5Name = "Sky";
-export const miscColor5HexCode = "#7697e6";
+// 11
+export const miscColor5Name = "Chase Navy Accent";
+export const miscColor5HexCode = "#003B71";
 export const miscColor5Type = "";
-export const miscColor5TextColor = "#ffffff";
+export const miscColor5TextColor = "#FFFFFF";
 
-export const miscColor6Name = "Orange";
-export const miscColor6HexCode = "#f2952f";
+// 12
+export const miscColor6Name = "Chase Light Teal";
+export const miscColor6HexCode = "#59BDBB";
 export const miscColor6Type = "";
 export const miscColor6TextColor = "#000000";
 
-export const miscColor7Name = "Light Pink";
-export const miscColor7HexCode = "#e8555f";
+// 13
+export const miscColor7Name = "Chase Red Alert";
+export const miscColor7HexCode = "#D42D2D";
 export const miscColor7Type = "";
-export const miscColor7TextColor = "#ffffff";
+export const miscColor7TextColor = "#FFFFFF";
 
-export const miscColor8Name = "Dark Green";
-export const miscColor8HexCode = "#40714e";
+// 14
+export const miscColor8Name = "Chase Dark Green";
+export const miscColor8HexCode = "#1C5B46";
 export const miscColor8Type = "";
-export const miscColor8TextColor = "#ffffff";
+export const miscColor8TextColor = "#FFFFFF";
 
-export const miscColor9Name = "Brown";
-export const miscColor9HexCode = "#a97f55";
+// 15
+export const miscColor9Name = "Chase Brown Accent";
+export const miscColor9HexCode = "#6B4D3E";
 export const miscColor9Type = "";
-export const miscColor9TextColor = "#ffffff";
+export const miscColor9TextColor = "#FFFFFF";
